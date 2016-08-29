bfa9b8e5e619a5126ae7de4153ed9498e208bd34
shenzhaoxiang <shenzhaoxiang@gmail.com>
2016-08-29 19:49:23 +0800

    个人中心

--- app/src/main/java/com/ctsig/rdo/ui/main/fragment/MeFragment.java
+++ app/src/main/java/com/ctsig/rdo/ui/main/fragment/MeFragment.java
@@ -1,11 +1,17 @@
 package com.ctsig.rdo.ui.main.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.ctsig.rdo.R;
 import com.ctsig.rdo.base.BaseFragment;
+import com.ctsig.rdo.ui.login.activity.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
 /**
@@ -16,6 +22,9 @@ import com.ctsig.rdo.base.BaseFragment;
  * @date: 2016-05-27 17:20
  */
 public class MeFragment extends BaseFragment {
+    @BindView(R.id.btn_logout)
+    Button btnLogout;
+
     @Override
     public int bindLayout() {
         return R.layout.fragment_me;
@@ -29,5 +38,14 @@ public class MeFragment extends BaseFragment {
     @Override
     public void doBusiness(Context mContext) {
 
+    }
+
+
+    @OnClick(R.id.btn_logout)
+    public void onClick() {
+        Intent it = new Intent() ;
+        it.setClass(getActivity(), LoginActivity.class) ;
+        startActivity(it);
+        getActivity().finish();
     }
 }
